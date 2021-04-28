6df7d01b19bf3905f745af4da4336a1655b9e6d5
Hesham Omar <heshamomar820@gmail.com>
2021-04-28 02:38:33 +0200

    displaying reaction timer

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="block" v-if="showBlock">
+    <div class="block" v-if="showBlock" @click="stopTimer">
         Click me
     </div>
 </template>
@@ -10,12 +10,14 @@ export default {
     data(){
         return{
             showBlock: false,
+            timer: null,
+            reactionTime: 0
         }
     },
     mounted(){
-        console.log('mounted')
         setTimeout(() => {
             this.showBlock = true
+            this.startTimer()
         }, this.delay)
     },
     updated(){
@@ -23,13 +25,25 @@ export default {
     },
     unmounted(){
         console.log('unmounted')
+    },
+    methods: {
+        startTimer(){
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10);
+        },
+        stopTimer(){
+            clearInterval(this.timer)
+            console.log(this.reactionTime)
+            this.$emit('end', this.reactionTime)
+        }
     }
 }
 </script>
 
 <style>
     .block{
-        width: 400px;
+        width: clamp(200px, 300px, 400px);
         border-radius: 20px;
         background: #0faf87;
         color: #fff;
